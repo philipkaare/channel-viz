e6a1f992045273c69e96e466b70c8d36d8f58321
philipkaare <philip.kaare@gmail.com>
2015-04-28 15:50:35 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -149,8 +149,8 @@
 										width: 600,
 										height: 200,
 										renderer: 'scatterplot',
-										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										min: 0,
+										max: 100,
 										padding: {
 											top: 0.02,
 											right: 0.02,
